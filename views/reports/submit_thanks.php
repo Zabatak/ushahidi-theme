a1e7c6edcf61112bb53d586630e2effdd427aa72
Abbas Adel <abbas.adel@mufix.org>
2013-03-26 22:12:42 +0200

    update thank page

---
+++ views/reports/submit_thanks.php
@@ -0,0 +1,19 @@
+<div id="content">
+    <div class="content-bg">
+        <!-- start block -->
+        <div class="big-block">
+            <!-- green-box -->
+            <div class="alert alert-success">
+                <h4><?php echo Kohana::lang('ui_main.reports_submitted'); ?></h4>
+                <br />
+                <p><b >
+                <a href="<?php echo url::site() . 'reports' ?>"  class="fonty">
+                    <?php echo Kohana::lang('ui_main.reports_return'); ?></a>
+                    </b>
+                </p>
+
+            </div>
+        </div>
+        <!-- end block -->
+    </div>
+</div>
